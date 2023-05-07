049e95e794af7ed43bf709f2fc746de81360932b
miracle <wangwenhui3614@gmail.com>
2023-05-07 08:32:00 +0800

    update

--- lua/langs/clangd.lua
+++ lua/langs/clangd.lua
@@ -55,7 +55,7 @@ return {
               },
               -- on_attach = require("modules.lang").on_attach,
               capabilities = opts.capabilities,
-              single_file_support = false,
+              single_file_support = true,
             },
           }
           require("clangd_extensions").setup(clangd_opts)
