7d7fbe922758417af0fc2c83ff3ba752b71f1865
amermiracle <amermiracle@didiglobal.com>
2023-05-19 23:30:10 +0800

    tmux navi

--- lua/ui/colortheme.lua
+++ lua/ui/colortheme.lua
@@ -8,6 +8,54 @@ return {
   --     })
   --   end,
   -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      term_colors = true,
+      transparent_background = false,
+      no_italic = false,
+      no_bold = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      highlight_overrides = {
+        mocha = function(C)
+          return {
+            TabLineSel = { bg = C.pink },
+            CmpBorder = { fg = C.surface2 },
+            Pmenu = { bg = C.none },
+            TelescopeBorder = { link = "FloatBorder" },
+          }
+        end,
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      transparent = true,
+    },
+  },
   { "arturgoms/moonbow.nvim" },
   {
     "LazyVim/LazyVim",
